53b46f0d031e913663d7c4d8e5808888f4560ac9
Kuwado <viethoan557723@Ubuntu.myguest.virtualbox.org>
2024-11-17 21:40:49 +0700

    models

---
+++ src/models/Attendance.h
@@ -0,0 +1,32 @@
+#ifndef ATTENDANCE_H
+#define ATTENDANCE_H
+#include <iostream>
+using namespace std;
+
+class Attendance {
+  protected:
+    int id = 0;
+    int meeting_id;
+    int student_id;
+
+  public:
+    // Constructor
+    Attendance() {};
+    Attendance(const int &nmeeting_id, const int &nstudent_id) : meeting_id(nmeeting_id), student_id(nstudent_id) {}
+
+    // Getter
+    int getId() const { return id; }
+    int getMeetingId() const { return meeting_id; }
+    int getStudentId() const { return student_id; }
+
+    // Setter
+    void setId(const int &newId) { id = newId; }
+    void setTeacherId(const int &newMeetingId) { meeting_id = newMeetingId; }
+    void setTeacherId(const int &newStudentId) { student_id = newStudentId; }
+
+    void show() {
+        cout << "Attendance, Id: " << id << " ,meeting_id: " << meeting_id << " ,student_id: " << student_id << endl;
+    }
+};
+
+#endif
---
+++ src/repository/ReportRepository.h
@@ -0,0 +1,87 @@
+#ifndef REPORTREPOSITTORY_HH
+#define REPORTREPOSITTORY_HH
+
+#include "../../data/Database.h"
+#include "../models/Timeslot.h"
+#include "../models/User.h"
+#include "UserRepository.h"
+#include <cppconn/prepared_statement.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class TimeslotRepository {
+  private:
+    Database db;
+    UserRepository userRepo;
+
+  public:
+    TimeslotRepository() {}
+
+    void create(const Timeslot &timeslot) {
+        if (db.connect()) {
+            User user = userRepo.getUserById(timeslot.getTeacherId());
+            if (user.getId() == 0) {
+                cout << "User khong ton tai!" << endl;
+                return;
+            } else if (user.getRole() != "teacher") {
+                cout << "User khong phai giao vien!" << endl;
+                return;
+            }
+
+            string query = "INSERT INTO timeslots (start, end, date, type, teacher_id) VALUES (?, ?, ?, ?, ?)";
+            try {
+                sql::PreparedStatement *pstmt = db.getConnection()->prepareStatement(query);
+                pstmt->setString(1, timeslot.getStart());
+                pstmt->setString(2, timeslot.getEnd());
+                pstmt->setString(3, timeslot.getDate());
+                pstmt->setString(4, timeslot.getType());
+                pstmt->setInt(5, timeslot.getTeacherId());
+
+                pstmt->executeUpdate();
+                cout << "Khai bao thoi gian: " << timeslot.getStart() << " - " << timeslot.getEnd() << " ( "
+                     << timeslot.getDate() << " )" << endl;
+                delete pstmt;
+            } catch (sql::SQLException &e) {
+                cerr << "Loi them khe thoi gian: " << e.what() << endl;
+            }
+        } else {
+            cout << "Lỗi không thể truy cập cơ sở dữ liệu." << endl;
+        }
+    }
+
+    vector<Timeslot> getTimeslotsByTeacherId(const int &teacher_id) {
+        vector<Timeslot> timeslots;
+
+        if (db.connect()) {
+            string query = "SELECT * FROM timeslots WHERE teacher_id = ?";
+            try {
+                sql::PreparedStatement *pstmt = db.getConnection()->prepareStatement(query);
+                pstmt->setInt(1, teacher_id);
+                sql::ResultSet *res = pstmt->executeQuery();
+
+                while (res->next()) {
+                    Timeslot ts;
+                    ts.setId(res->getInt("id"));
+                    ts.setStart(res->getString("start"));
+                    ts.setEnd(res->getString("end"));
+                    ts.setDate(res->getString("date"));
+                    ts.setType(res->getString("type"));
+                    ts.setTeacherId(res->getInt("teacher_id"));
+                    timeslots.push_back(ts);
+                }
+                delete res;
+                delete pstmt;
+            } catch (sql::SQLException &e) {
+                std::cerr << "Lỗi khi lấy dữ liệu từ timeslots: " << e.what() << std::endl;
+            }
+        } else {
+            cout << "Lỗi không thể truy cập cơ sở dữ liệu." << endl;
+        }
+
+        return timeslots;
+    }
+};
+
+#endif
